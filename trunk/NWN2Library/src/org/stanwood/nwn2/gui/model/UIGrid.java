74a617cf2489bfc8839bcefee7f80614b7928566
jp.stanford <jp.stanford@d8cdcf74-1a06-11df-b433-597f7f7f106e>
2010-02-16 22:35:51 +0000

    More updates to the object model

--- trunk/NWN2Library/src/org/stanwood/nwn2/gui/model/UIGrid.java
+++ trunk/NWN2Library/src/org/stanwood/nwn2/gui/model/UIGrid.java
@@ -4,8 +4,46 @@ public class UIGrid extends UIObject {
 
 	private static final long serialVersionUID = -7779759025081098331L;
 
+	private Integer rows;
+	private Integer columns;
+	private Integer yPadding = 0;
+	private Integer xPadding= 0;
+	
 	public UIGrid(NWN2GUIObject parent) {
 		super(parent);
 	}
 
+	public Integer getRows() {
+		return rows;
+	}
+
+	public void setRows(Integer rows) {
+		this.rows = rows;
+	}
+
+	public Integer getColumns() {
+		return columns;
+	}
+
+	public void setColumns(Integer columns) {
+		this.columns = columns;
+	}
+
+	public Integer getyPadding() {
+		return yPadding;
+	}
+
+	public void setyPadding(Integer yPadding) {
+		this.yPadding = yPadding;
+	}
+
+	public Integer getxPadding() {
+		return xPadding;
+	}
+
+	public void setxPadding(Integer xPadding) {
+		this.xPadding = xPadding;
+	}
+
+	
 }
